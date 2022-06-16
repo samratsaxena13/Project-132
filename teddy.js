32adab053db4473517b6e578c0137ef60109dca5
samratsaxena13 <102512118+samratsaxena13@users.noreply.github.com>
2022-06-16 11:48:16 +0530

    Add files via upload

---
+++ teddy.js
@@ -0,0 +1,35 @@
+var img = "";
+var statusweb = "";
+
+function preload() {
+    img = loadImage("teddy.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(600, 400);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("statusLbl").innerHTML = "Detecting..";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    statusweb = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if(error) {
+        console.error(error);
+    } else {
+        console.log(results);
+    }
+}
+
+function draw() {
+    image(img, 0, 0, 600, 400);
+}
+
+function goBack() {
+    window.location = "index.html"
+}
